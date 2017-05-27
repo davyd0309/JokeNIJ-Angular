61b0674026c98c05512d7f725d13d6032127010e
Konrad <konrad@dawydiuk.pl>
2017-05-27 13:29:31 +0200

    #777 - implements login function

--- app/app.js
+++ app/app.js
@@ -1,15 +1,51 @@
-'use strict';
+(function () {
+  'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.login',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+  angular
+      .module('app', ['ngRoute', 'ngCookies'])
+      .config(config)
+      .run(run);
 
-  $routeProvider.otherwise({redirectTo: '/login'});
-}]);
+  config.$inject = ['$routeProvider', '$locationProvider'];
+  function config($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/', {
+          controller: 'HomeController',
+          templateUrl: 'home/HomeView.html',
+          controllerAs: 'vm'
+        })
+
+        .when('/login', {
+          controller: 'LoginController',
+          templateUrl: 'login/LoginView.html',
+          controllerAs: 'vm'
+        })
+
+        .when('/register', {
+          controller: 'RegisterController',
+          templateUrl: 'register/RegisterView.html',
+          controllerAs: 'vm'
+        })
+
+        .otherwise({ redirectTo: '/login' });
+  }
+
+  run.$inject = ['$rootScope', '$location', '$cookies', '$http'];
+  function run($rootScope, $location, $cookies, $http) {
+    // keep user logged in after page refresh
+    $rootScope.globals = $cookies.getObject('globals') || {};
+    if ($rootScope.globals.currentUser) {
+      $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata;
+    }
+
+    $rootScope.$on('$locationChangeStart', function (event, next, current) {
+      // redirect to login page if not logged in and trying to access a restricted page
+      var restrictedPage = $.inArray($location.path(), ['/login', '/register']) === -1;
+      var loggedIn = $rootScope.globals.currentUser;
+      if (restrictedPage && !loggedIn) {
+        $location.path('/login');
+      }
+    });
+  }
+
+})();
---
+++ app/register/RegisterController.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Judyta on 27.05.2017.
+ */
+
+
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('RegisterController', RegisterController);
+
+    RegisterController.$inject = ['UserService', '$location', '$rootScope', 'FlashService'];
+    function RegisterController(UserService, $location, $rootScope, FlashService) {
+        var vm = this;
+
+        vm.register = register;
+
+        function register() {
+            vm.dataLoading = true;
+            UserService.Create(vm.user)
+                .then(function (response) {
+                    if (response.success) {
+                        FlashService.Success('Registration successful', true);
+                        $location.path('/login');
+                    } else {
+                        FlashService.Error(response.message);
+                        vm.dataLoading = false;
+                    }
+                });
+        }
+    }
+
+})();
